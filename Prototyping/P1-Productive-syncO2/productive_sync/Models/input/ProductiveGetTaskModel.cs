0a19f2388d638ba3f47b0ac5327773c17e72d945
Daan Receveur <daan@bluenotion.nl>
2024-08-14 15:46:37 +0200

    removed documentation irrelevant files

--- Prototyping/P1-Productive-syncO2/productive_sync/Models/input/ProductiveGetTaskModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-namespace productive_sync.Models;
-public class ProductiveGetTaskModel
-{
-    public Data? Data { get; set; }
-    public Meta? Meta { get; set; }
-}
